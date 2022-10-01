18ac257a5af1cd39c8c0d1f07a31b805de228a97
connorbelding <the_connn@hotmail.com>
2022-09-30 23:44:37 -0400

    Added logic to change grid cell color using custom HTML attributes

--- index.js
+++ index.js
@@ -7,13 +7,25 @@ function generateGrid(size = 16) {
         row.classList.add('grid-row');
         for (let j = 0; j < size; j++) {
             const cell = document.createElement('div');
-            cell.classList.add('grid-cell')
+            cell.classList.add('grid-cell');
+            cell.setAttribute('data-brightness', '1');
+            cell.style.backgroundColor = '#eee';
+            cell.addEventListener('pointerenter', handlePointerEnter);
             row.appendChild(cell);
         }
         container.appendChild(row);
     }
     const gridArea = document.getElementById('grid-area');
     gridArea.appendChild(container);
+}
+
+function handlePointerEnter(e) {
+    const brightness = Number(e.target.getAttribute('data-brightness'));
+
+    if (brightness > 0) {
+        e.target.style.filter = `brightness(${brightness - 0.1})`;
+        e.target.setAttribute('data-brightness', `${brightness - 0.1}`)
+    };
 }
 
 generateGrid()
